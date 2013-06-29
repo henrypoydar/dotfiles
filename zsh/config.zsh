313c225981aaee94c446c847c3bb51081d7868f0
Henry Poydar <henry@poydar.com>
2013-06-29 17:07:42 -0400

    Use homebrew python version is used for non-homebrew applications

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,3 +1,4 @@
 export EDITOR='vim'
 export PATH="$HOME/bin:$HOME/.bin:$/HOME/.vim/bin:/usr/local/homebrew/bin:/usr/local/bin:/usr/local/git/libexec/git-core:/usr/local/sbin:/usr/local/mysql/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export export PYTHONPATH="/usr/local/lib/python2.7/site-packages:$PYTHONPATH"
