7ee374a391cd8b8e1486a220f472bd25462de363
Henry Poydar <hpoydar@gmail.com>
2014-05-08 16:43:28 -0400

    Modify prompt depending on Vi mode

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -19,8 +19,22 @@ function ruby_prompt_info() {
   rbenv version | awk '{print $1}'
 }
 
-local return_code='%(?,%F{gray}▸,%F{red}▸)'
+local vi_insert_indicator='%F{white}▸'
+local vi_normal_indicator='%F{gray}▸'
+local return_code="%(?,${vi_insert_indicator},%F{red}▸)"
 
 setopt prompt_subst
+
+# Default prompt
 PROMPT='${return_code} %F{white}%. %F{green}${vcs_info_msg_0_}%{$reset_color%}%B$%b '
 RPROMPT='%F{red}$(ruby_prompt_info)%{$reset_color%}'
+
+# Change the prompt for Vi modes
+function zle-line-init zle-keymap-select {
+  local_vi_mode="${${KEYMAP/vicmd/${vi_normal_indicator}}/(main|viins)/${vi_insert_indicator}}"
+  PROMPT='${local_vi_mode} %F{white}%. %F{green}${vcs_info_msg_0_}%{$reset_color%}%B$%b '
+  zle reset-prompt
+}
+
+zle -N zle-line-init
+zle -N zle-keymap-select
