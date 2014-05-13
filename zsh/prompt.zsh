bef52f2ca65cd9f8994c503cd0c75d8341c65cdc
Henry Poydar <hpoydar@gmail.com>
2014-05-13 11:36:00 -0400

    Prompt change; nokogiri fix

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -19,8 +19,8 @@ function ruby_prompt_info() {
   rbenv version | awk '{print $1}'
 }
 
-local vi_insert_indicator='%F{white}▸'
-local vi_normal_indicator='%F{gray}▸'
+local vi_insert_indicator='%F{gray}▸'
+local vi_normal_indicator='%F{white}▸'
 local return_code="%(?,${vi_insert_indicator},%F{red}▸)"
 
 setopt prompt_subst
@@ -34,7 +34,7 @@ function zle-line-init zle-keymap-select {
   local_vi_mode="${${KEYMAP/vicmd/${vi_normal_indicator}}/(main|viins)/${vi_insert_indicator}}"
   PROMPT='${local_vi_mode} %F{white}%. %F{green}${vcs_info_msg_0_}%{$reset_color%}%B$%b '
   zle reset-prompt
-}
+  }
 
 zle -N zle-line-init
 zle -N zle-keymap-select
